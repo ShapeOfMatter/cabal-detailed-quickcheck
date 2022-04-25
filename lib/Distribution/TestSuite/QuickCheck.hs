66f25846f7a4c57a1538b814e356dec114edfe74
Anselm Schüler <mail@anselmschueler.com>
2022-04-25 12:40:57 +0200

    Fix typo in doc-comment

--- lib/Distribution/TestSuite/QuickCheck.hs
+++ lib/Distribution/TestSuite/QuickCheck.hs
@@ -338,6 +338,6 @@ getPropertyTest = getPropertyTestWithUsing stdTestArgs . discardingTestArgs
 getPropertyTests :: QC.Testable prop => [PropertyTest prop] -> [T.Test]
 getPropertyTests = (getPropertyTest <$>)
 
--- | Get a named test group from a list of 'PropertyTests's. These are assumed to be able to run in parallel. See 'T.testGroup' and 'T.Group'.
+-- | Get a named test group from a list of 'PropertyTest's. These are assumed to be able to run in parallel. See 'T.testGroup' and 'T.Group'.
 propertyTestGroup :: QC.Testable prop => String -> [PropertyTest prop] -> T.Test
 propertyTestGroup name = T.testGroup name . getPropertyTests
